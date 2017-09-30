299a1f96325d1c14124bd8768e9863a2277e4259
XIAN LU <aikolu@hotmail.com>
2017-09-29 17:18:42 -0700

    Merge remote-tracking branch 'origin/XinyuShao' into XL-firstlevel

--- TheDistance/Assets/Scripts/Player.cs
+++ TheDistance/Assets/Scripts/Player.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 [RequireComponent(typeof(Controller2D))]
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
+	//predefined names
+	public string EricWorldName = "EricWorld";
+	public string NatalieWorldName = "NatalieWorld";
+	public string EricPosName = "EricPos";
+	public string NataliePosName = "NataliePos";
+	public string ShareWorldName="ShareWorld";
+
+	public GameObject spirit;
+	public Vector3 spiritTargetPos;
+
+	public float interpolateTime = 10;
 
     public float jumpHeight;
     public float timeToJumpApex;
@@ -42,55 +54,114 @@ public class Player : MonoBehaviour
         
         //camera
         offset = Camera.main.transform.position - transform.position;
+
+		//find root, because spirit is deactived, so we can only use transform to find it.
+		GameObject root = GameObject.Find("Root");
+		spirit = root.transform.Find("Spirit").gameObject;
+		spiritTargetPos = spirit.transform.position;
+
+		Transform EricTransform;//EricPos
+		Transform NatalieTransform;//NataliePos
+		EricTransform = root.transform.Find(ShareWorldName + "/" + EricPosName);
+		NatalieTransform = root.transform.Find(ShareWorldName + "/" + NataliePosName);
+
+		if (isLocalPlayer && isServer) {
+			transform.position = EricTransform.position;
+		}
+		if (isLocalPlayer && !isServer) {
+			transform.position = NatalieTransform.position;
+		}
+
+		//
+		root.transform.Find (EricWorldName).gameObject.SetActive (isServer);
+		root.transform.Find (NatalieWorldName).gameObject.SetActive (!isServer);
+
+		//hideRemotePlayer
+		if (!isLocalPlayer) {
+			gameObject.SetActive (false);
+		} else {
+			gameObject.name = "Player";
+		}
+
+		//when client(Natalie) is connected and created, initialize server and itself
+		if (!isServer && isLocalPlayer)
+		{
+			CmdInitializeServer(NatalieTransform.position,EricTransform.position);
+			InitializeClient(EricTransform.position,NatalieTransform.position);
+		}
     }
 
     void Update()
     {
-        // press Q to interact with the object
-        if(Input.GetKey(KeyCode.Q))
-           controller.collisions.interact = true;
-        else
-           controller.collisions.interact = false;
-
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            print("have NPC in range? " + (curNPC != null));
-            if(curNPC != null)
-            {
-                print("NPC says: " + curNPC.NPCtalk);
-            }
-        }
-
-        // on the ground
-        if (controller.collisions.above || controller.collisions.below || controller.collisions.onLadder)
-        {
-            velocity.y = 0;
-        }
-
-        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        //print("On the ladder? " + controller.collisions.onLadder);
-
-        // move in y axis if on the ladder
-        if(controller.collisions.onLadder)
-        {
-            //print("User on the ladder!");
-            //print(controller.collisions.onLadder);
-            velocity.y = input.y * moveSpeed;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && (controller.collisions.below && !controller.collisions.onLadder))
-        {
-            velocity.y = jumpVelocity;
-        }
-
-        velocity.x = input.x * moveSpeed;
-        if(!controller.collisions.onLadder) velocity.y -= gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+		//input controlling move
+		KeyControlMove();
 
         //camera
         Vector3 tmp = new Vector3(0, 0, offset.z);
-        Camera.main.transform.position = transform.position + tmp;
+
+		if (isLocalPlayer) {
+			Camera.main.transform.position = transform.position + tmp;
+		}
+
+
+		//interpolate move by frame rate, when position not equal, move
+		if (!spirit.transform.position.Equals(spiritTargetPos))
+		{
+			spirit.transform.Translate((spiritTargetPos - spirit.transform.position) / interpolateTime);
+		}
     }
+	void KeyControlMove(){
+		// press Q to interact with the object
+		if(Input.GetKey(KeyCode.Q))
+			controller.collisions.interact = true;
+		else
+			controller.collisions.interact = false;
+
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			print("have NPC in range? " + (curNPC != null));
+			if(curNPC != null)
+			{
+				print("NPC says: " + curNPC.NPCtalk);
+			}
+		}
+
+		// on the ground
+		if (controller.collisions.above || controller.collisions.below || controller.collisions.onLadder)
+		{
+			velocity.y = 0;
+		}
+
+		Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		//print("On the ladder? " + controller.collisions.onLadder);
+
+		// move in y axis if on the ladder
+		if(controller.collisions.onLadder)
+		{
+			//print("User on the ladder!");
+			//print(controller.collisions.onLadder);
+			velocity.y = input.y * moveSpeed;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space) && (controller.collisions.below && !controller.collisions.onLadder))
+		{
+			velocity.y = jumpVelocity;
+		}
+
+		velocity.x = input.x * moveSpeed;
+		if(!controller.collisions.onLadder) velocity.y -= gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
+
+		if (isServer)
+		{
+			RpcMove(transform.position);
+		}
+		else
+		{
+			//print("update cmd move");
+			CmdMove(transform.position);
+		}
+	}
 
     void backToCheckPoint()
     {
@@ -103,4 +174,41 @@ public class Player : MonoBehaviour
         backToCheckPoint();
         //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+	//sent by server, run on all clients
+	[ClientRpc]
+	public void RpcMove(Vector3 pos)
+	{
+		//print("Rpc Move");
+		if (!isServer)
+		{
+			GameObject.Find("Player").GetComponent<Player>().spiritTargetPos = pos;
+		}
+	}
+
+	//sent by client, run on server
+	[Command]
+	public void CmdMove(Vector3 pos)
+	{
+		//print("Cmd Move");
+		GameObject.Find("Player").GetComponent<Player>().spiritTargetPos = pos;
+	}
+	[Command]
+	public void CmdInitializeServer(Vector3 spirit_pos, Vector3 player_pos)
+	{
+		//print("CmdIniatiateServer");
+		spirit.transform.position = spirit_pos;
+
+		spirit.SetActive(true);
+		GameObject.Find("Player").transform.position = player_pos;
+		GameObject.Find("Player").GetComponent<Player>().spiritTargetPos = spirit_pos;
+	}
+	public void InitializeClient(Vector3 spirit_pos, Vector3 player_pos)
+	{
+		//print("IniatiateClient");
+		spirit.transform.position = spirit_pos;
+		spiritTargetPos = spirit_pos;
+		spirit.SetActive(true);
+		GameObject.Find("Player").transform.position = player_pos;
+	}
 }
---
+++ TheDistance/Assets/Scripts/PlayerLAN.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerLAN : NetworkBehaviour
+{
+    public float speed = 4.0f;
+    public GameObject spirit;
+    public Vector3 spiritTargetPos;
+    public float interpolateTime = 10;
+
+    //predefined names
+    public string EricWorldName = "EricWorld";
+    public string NatalieWorldName = "NatalieWorld";
+    public string EricPosName = "EricPos";
+    public string NataliePosName = "NataliePos";
+
+    void Start () {
+        GameObject root = GameObject.Find("Root");
+        Transform EricTransform;//EricPos
+        Transform NatalieTransform;//NataliePos
+        if (isServer)
+        {
+            EricTransform = root.transform.Find(EricWorldName+"/"+EricPosName);
+            NatalieTransform = root.transform.Find(EricWorldName + "/" + NataliePosName);
+        }
+        else
+        {
+            EricTransform = root.transform.Find(NatalieWorldName + "/" + EricPosName);
+            NatalieTransform = root.transform.Find(NatalieWorldName + "/" + NataliePosName);
+        }
+
+        //find root, because spirit is deactived, so we can only use transform to find it.
+        spirit = root.transform.Find("Spirit").gameObject;
+        spiritTargetPos = spirit.transform.position;
+
+        //set up player game object name and remote player's name.
+        if (isLocalPlayer)
+        {
+            gameObject.name = "Player";
+        }
+        else
+        {
+            gameObject.name = "RemotePlayer";
+        }
+
+        //when client(Natalie) is connected and created, initialize server and itself
+        if (!isServer && isLocalPlayer)
+        {
+            CmdInitializeServer(NatalieTransform.position,EricTransform.position);
+            InitializeClient(EricTransform.position,NatalieTransform.position);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //print("IsLocalPlayer:" + isLocalPlayer);
+
+        //input controlling move
+        KeyControlMove();
+
+        //interpolate move by frame rate, when position not equal, move
+        if (!spirit.transform.position.Equals(spiritTargetPos))
+        {
+            spirit.transform.Translate((spiritTargetPos - spirit.transform.position) / interpolateTime);
+        }
+    }
+    
+
+    //sent by server, run on all clients
+    [ClientRpc]
+    public void RpcMove(Vector3 pos)
+    {
+        //print("Rpc Move");
+        if (!isServer)
+        {
+            GameObject.Find("Player").GetComponent<PlayerLAN>().spiritTargetPos = pos;
+        }
+    }
+
+    //sent by client, run on server
+    [Command]
+    public void CmdMove(Vector3 pos)
+    {
+        //print("Cmd Move");
+        GameObject.Find("Player").GetComponent<PlayerLAN>().spiritTargetPos = pos;
+    }
+    [Command]
+    public void CmdInitializeServer(Vector3 spirit_pos, Vector3 player_pos)
+    {
+        //print("CmdIniatiateServer");
+        spirit.transform.position = spirit_pos;
+
+        spirit.SetActive(true);
+        GameObject.Find("RemotePlayer").SetActive(false);
+        GameObject.Find("Player").transform.position = player_pos;
+        GameObject.Find("Player").GetComponent<PlayerLAN>().spiritTargetPos = spirit_pos;
+    }
+    public void InitializeClient(Vector3 spirit_pos, Vector3 player_pos)
+    {
+        //print("IniatiateClient");
+        spirit.transform.position = spirit_pos;
+        spiritTargetPos = spirit_pos;
+        spirit.SetActive(true);
+        GameObject.Find("RemotePlayer").SetActive(false);
+        GameObject.Find("Player").transform.position = player_pos;
+    }
+
+    void KeyControlMove()
+    {
+        float move = speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-move, 0, 0);
+            if (isServer)
+            {
+                RpcMove(transform.position);
+            }
+            else
+            {
+                //print("update cmd move");
+                CmdMove(transform.position);
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(move, 0, 0);
+            if (isServer)
+            {
+                RpcMove(transform.position);
+            }
+            else
+            {
+                //print("update cmd move");
+                CmdMove(transform.position);
+            }
+        }
+    }
+}

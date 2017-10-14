4f47b91146ffa874be565b3c51ab0480688bef07
BochangWu <bochangw@usc.edu>
2017-10-14 11:24:58 -0700

    fixed bug

--- TheDistance/Assets/Resources/Scripts/HarmfulTrigger.cs
+++ TheDistance/Assets/Resources/Scripts/HarmfulTrigger.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class HarmfulTrigger : MonoBehaviour {
 
-    private void Start()
+    int cnt = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Debug.Log("Something here");
+        {
+            cnt++;
+            print(gameObject.name + " hits the player!");
+            if(cnt == 2)
+            {
+                Player p = collision.GetComponent<Player>();
+                p.Die();
+                // when player falls, the player will die
+                // future lethal triggers can use this function
+            }
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
-        {
-            Player p = collision.GetComponent<Player>();
-            p.Die();
-            // when player falls, the player will die
-            // future lethal triggers can use this function
-        }
+        if (collision.gameObject.tag == "Player")
+            cnt--;
     }
 
 }

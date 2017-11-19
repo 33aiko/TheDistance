573f1e879393535dcfc25a95ca2868801aa66f07
iiidiot <nothing0n@foxmail.com>
2017-11-19 03:28:45 -0800

    add row boat

---
+++ TheDistance/Assets/Scripts/Items/RowBoat.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowBoat : MonoBehaviour {
+    public float forceX = 10;
+    public float forceY = 10;
+    Rigidbody r;
+	// Use this for initialization
+	void Start () {
+        r = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (r != null)
+        {
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                r.AddForce(forceX, -forceY, 0);
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                r.AddForce(forceX, forceY, 0);
+            }
+        }
+	}
+}

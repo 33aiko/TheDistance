f78b0791a66841a3ebcbb3e5c05244c493f6e40a
BochangWu <bochangw@usc.edu>
2018-04-17 22:45:53 -0700

    puuush sfx controller

--- TheDistance/Assets/Scripts/UI/VolumeMixer.cs
+++ TheDistance/Assets/Scripts/UI/VolumeMixer.cs
@@ -1,16 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
+using DG.Tweening;
 
 public class VolumeMixer : MonoBehaviour {
 
 	public AudioMixerGroup mixerGroup;
 
+    public GameObject slider;
+    public Sprite SFXIcon;
+    public Sprite SFXIcon_Mute;
+    public Image m_button;
+
+    bool sliderHidden = true;
+    float showTime = 5.0f;
+
+    private void Start()
+    {
+        HideSlider();
+    }
+    public void ToggleSlider()
+    {
+        if (sliderHidden)
+            ShowSlider();
+        else
+            HideSlider();
+    }
+    
+    public void HideSlider()
+    {
+        slider.SetActive(false);
+        sliderHidden = true;
+    }
+
+    public void ShowSlider()
+    {
+        slider.SetActive(true);
+        sliderHidden = false;
+        nextHideTime = Time.time + showTime;
+    }
+
     public void SetMixerGroupVolume(float f)
     {
         if (isMute) isMute = false;
+        float prev;
+        mixerGroup.audioMixer.GetFloat("masterVolume", out prev);
         mixerGroup.audioMixer.SetFloat("masterVolume", f);
+        nextHideTime = Time.time + showTime;
+        if(f == -80)
+        {
+            m_button.sprite = SFXIcon_Mute;
+        }
+        else
+        {
+            m_button.sprite = SFXIcon;
+        }
+    }
+
+    float nextHideTime = -1;
+    private void Update()
+    {
+        if(!sliderHidden)
+        {
+            if(Time.time >= nextHideTime)
+            {
+                HideSlider();
+            }
+        }
     }
 
     public void SetAtomVolume(float f)

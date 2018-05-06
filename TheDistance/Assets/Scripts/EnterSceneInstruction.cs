0ddcf11f1d2197ab34cc5a54fc039e1063d8ba9a
XIAN LU <aikolu@hotmail.com>
2018-05-06 13:41:26 -0700

    push push

--- TheDistance/Assets/Scripts/EnterSceneInstruction.cs
+++ TheDistance/Assets/Scripts/EnterSceneInstruction.cs
@@ -58,7 +58,7 @@ public class EnterSceneInstruction : MonoBehaviour {
 	}
 
 	void Update () {
-		if (Input.GetButtonDown ("Share") && isActivated) {
+		if (Input.GetKeyDown(KeyCode.V) && isActivated) {
 			ui_background.DOFade(0, fade_time);
 			ui_background.transform.DOScaleY(0f, fade_time);
 			ui_text.DOFade (0, fade_time);
